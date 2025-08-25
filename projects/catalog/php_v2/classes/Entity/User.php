a515f38e79cb5c4660bc67515fd4e3d2630d04fa
mediumrare55 <vansagain@mail.ru>
2025-08-25 13:50:16 +0600

    Create User.php

---
+++ projects/catalog/php_v2/classes/Entity/User.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class User{
+    public int $id;
+    public string $name;
+    public string $surname;
+    public ?string $patronymic;
+    public int $age;
+    public string $email;
+    public string $adress;
+
+
+public function __constract(
+    int $id,
+    string $name,
+    string $surname,
+    ?string $patronymic,
+    int $age,
+    string $email,
+    string $adress
+    
+) {
+    $this->id=$id;
+    $this->name=$name;
+    $this->surname=$surname;
+    $this->patronymic=$patronymic;
+    $this->age=$age;
+    $this->email=$email;
+    $this->adress=$adress;
+
+}
+public function toArray(): array {
+    return [
+        'id' => $this->id,
+        'name' => $this->name,
+        'patronymic' => $this->patronymic,
+        'age' => $this->age,
+        'email' => $this->email,
+        'adress' => $this->adress
+    ];
+}
+
+}
