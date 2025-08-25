fa62ddc92cf0509a38d3111da22636498bf9096f
mediumrare55 <vansagain@mail.ru>
2025-08-25 13:53:20 +0600

    Update 001_create_products.py

--- projects/catalog/backend-python/migrations/001_create_products.py
+++ projects/catalog/backend-python/migrations/001_create_products.py
@@ -1,46 +1,49 @@
-# backend-python/migrations/001_create_products.py
 import sqlite3
 
-# подключаемся к файлу БД (если файла нет – создастся автоматически) Здесь был Иван
-conn = sqlite3.connect("../product_catalog.db")
+conn = sqlite3.connect('../database.db')
 cursor = conn.cursor()
 
-# создаём таблицу categories
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS categories (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
-);
+
+cursor.executescript("""
+    CREATE TABLE IF NOT EXISTS categories (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS brands (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        description TEXT,
+        category_id INTEGER,
+        brand_id INTEGER,
+        price REAL,  
+        stock INTEGER,
+        FOREIGN KEY (category_id) REFERENCES categories(id),
+        FOREIGN KEY (brand_id) REFERENCES brands(id)
+    );
 """)
 
-# создаём таблицу products
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS products (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    description TEXT,
-    price REAL NOT NULL,
-    image_url TEXT,
-    category_id INTEGER,
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-);
-""")
 
-# наполняем categories
-cursor.executemany("INSERT INTO categories (name) VALUES (?)", [
-    ("Electronics",), ("Books",)
-])
 
-# наполняем products начальными данными
-cursor.executemany("""
-INSERT INTO products (name, description, price, image_url, category_id)
-VALUES (?, ?, ?, ?, ?)
-""", [
-    ("Smartphone XYZ", "Описание смартфона XYZ", 19999.99, None, 1),
-    ("Book 'Learn Python'", "Учебник по Python для начинающих", 499.00, None, 2)
-])
 
-conn.commit()
-conn.close()
+# Добавляем категории
+cursor.execute("INSERT INTO categories (name) VALUES ('Одежда')"),
+cursor.execute("INSERT INTO categories (name) VALUES ('Обувь')"),
+cursor.execute("INSERT INTO categories (name) VALUES ('Аксессуары')"),
 
-print("Migration 001 applied successfully (SQLite).")
+# Добавляем бренды
+cursor.execute("INSERT INTO brands (name) VALUES ('Nike')"),
+cursor.execute("INSERT INTO brands (name) VALUES ('Adidas')"),
+cursor.execute("INSERT INTO brands (name) VALUES ('RayBan')"),
+
+# Добавляем товары
+cursor.execute("""INSERT INTO products (name, description, price, stock, category_id, brand_id) VALUES ('Футболка хлопковая Оверсайз', 'Самая модная футболка гадом буду', 1499.00, 3,1,1)"""),
+cursor.execute("""INSERT INTO products (name, description, price, stock, category_id, brand_id) VALUES ('Футбольные бутсы','Магаа, это лучшие бутсы, гадом буду. Месси на кампноу в них играл',11990,6,2,2)"""),
+cursor.execute("""INSERT INTO products (name,description,price,stock,category_id,brand_id) VALUES ('Очки солнцезащитные','Просто. Красиво. Удобно.',21299,17,3,3)"""),
+
+print("Миграция успешно завершена")
